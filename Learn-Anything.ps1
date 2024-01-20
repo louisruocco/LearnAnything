187b10d431e3aebe3419e80c140d5e7a1ea3157b
Louis Ruocco <louisruocco1@gmail.com>
2024-01-20 21:46:48 +0000

    able to send the email with the payload now

--- Learn-Anything.ps1
+++ Learn-Anything.ps1
@@ -3,20 +3,52 @@ function Find {
         [Parameter(Mandatory)]
         [string]$topic
     )
-    $apiKey = Get-Content 'E:\Code\Learn Anything\utils\secrets.txt'
+    $apiKey = (Get-Content 'E:\Code\Learn Anything\utils\secrets.txt')[0]
     $endpoint = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=$topic&type=video&key=$apiKey"
     $res = Invoke-RestMethod $endpoint
     $items = $res.items
-    foreach($item in $items){
+    $results = foreach($item in $items){
         $titles = $item.snippet.title
         $ids = $item.id.videoId
         $table = @{ $titles = $ids }
         foreach($video in $table){
             $keys = $video.Keys
             $values = $video.Values
-            write-host "$keys | https://www.youtube.com/watch?v=$values"
+            "$keys | <a href = 'https://www.youtube.com/watch?v=$values'>https://www.youtube.com/watch?v=$values</a>"
         }
     }
+
+    Send-Email
 }
 
-Find 
+function Send-Email {
+    $username = (Get-Content '.\utils\secrets.txt')[1]
+    $password = (Get-Content '.\utils\secrets.txt')[2] | ConvertTo-SecureString -AsPlainText -Force
+    $emailAddress = (Get-Content '.\utils\secrets.txt')[3]
+    $videos = foreach($video in $results){
+        "<li>$video</li>"
+    }
+
+    $body = @"
+    <h1>Learning Resources for $topic</h1>
+    <p>Please find some learning resources that could be useful on $topic</p>
+    <ul>
+        $videos
+    </ul>
+
+
+"@
+    $email = @{
+        from = $username
+        to = $emailAddress
+        subject = "Search Results for $topic"
+        smtpserver = "smtp.gmail.com"
+        body = $body
+        port = 587
+        credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+        usessl = $true
+        verbose = $true
+    }
+
+    Send-MailMessage @email -BodyAsHtml
+}
