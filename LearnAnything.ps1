a86bd3377d495da74720c149c6663e987eb8f70f
Louis Ruocco <louisruocco1@gmail.com>
2024-02-02 17:11:19 +0000

    removed warning message to allow it to silently continue

--- LearnAnything.ps1
+++ LearnAnything.ps1
@@ -3,6 +3,8 @@ function Find {
         [Parameter(Mandatory)]
         [string]$topic
     )
+
+    Write-Output "Finding results based on $topic..."
     $path = resolve-path $PSScriptRoot\utils
     Set-location $path
     $apiKey = Import-Clixml -Path $PSScriptRoot\utils\apikey.xml | ConvertFrom-SecureString -AsPlainText
@@ -20,11 +22,11 @@ function Find {
         }
     }
 
+    Write-Host "Sending results now!"
     $username = Import-Clixml -Path "$PSScriptRoot\utils\username.xml" | ConvertFrom-SecureString -AsPlainText
     $emailAddress = Import-Clixml -Path "$PSScriptRoot\utils\emailAddress.xml" | ConvertFrom-SecureString -AsPlainText
     $password =  Import-Clixml -Path "$PSScriptRoot\utils\password.xml"
     $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
-    $cred.UserName
     $videos = foreach($video in $results){
         "<li>$video</li>"
     }
@@ -51,8 +53,9 @@ function Find {
         verbose = $true
     }
 
-    Send-MailMessage @email -BodyAsHtml
+    Send-MailMessage @email -BodyAsHtml -WarningAction 0
+
+    set-location $HOME
 }
 
 Find
-
